c5b75a9aa81cf15c118d7efcdad7f4bd2aad7433
RockThunderheart <alexdaigre@gmail.com>
2018-05-01 21:42:39 -0500

    added additional hashing methods

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      allHashesFor1: {},
     }
   }
 
   componentDidMount() {
-      this.createHash1();
+      this.createHash("problem1Hashes", this.hashMethod1);
+      this.createHash("LinearProbingHash", this.hashMethodLinearProbing, 1);
+      this.createHash("QuadraticProbingHash", this.hashMethodQuadraticProbing, 1);
+      this.createHash("DoubleHashingHash", this.hashMethodDoubleHashing, 1);
   }
 
-  createHash1 = () => {
-    const primeSet = Array.from(this.generatePrimeSet(5));
+  createHash = (hashCollectionName, hashMethod, numberOfHashes = 5) => {
+    const primeSet = Array.from(this.generatePrimeSet(numberOfHashes));
     console.log(primeSet);
-    const allHashesFor1 = {};
+    const allHashes = {};
 
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i < numberOfHashes; i++){
       console.log(`loop ${i}`);
       const newHash = {};
       const newHashCollisions = {};
@@ -26,19 +28,43 @@ class App extends Component {
       for(let j = 0; j < 25; j++){
         const newValue = this.getRandomIntInclusive(1, 20000);
         console.log(`Value ${j}: ${newValue}`);
-        const newValueHash = newValue % primeSet[i];
+        const newValueHash = hashMethod(newValue, primeSet[i], newHash);
         console.log(`Key ${j}: ${newValueHash}`);
 
         newHash.hasOwnProperty(newValueHash) ?
         newHashCollisions[newValueHash] = newValue :
         newHash[newValueHash] = newValue;
       }
-      allHashesFor1[primeSet[i]] = {hash: newHash, HashCollisions: newHashCollisions };
+      allHashes[primeSet[i]] = {hash: newHash, HashCollisions: newHashCollisions };
     }
-    this.setState({
-      allHashesFor1: allHashesFor1,
-    });
+    const newStateObject = {[hashCollectionName]: allHashes,}
+    this.setState(newStateObject);
   };
+
+  hashMethod1 = (value, prime, currentHash) => {
+    return value % prime;
+  }
+
+  hashMethodLinearProbing = (value, prime, currentHash) => {
+    let i = 0;
+    let hashValue;
+    do{
+      hashValue = (value + i) % prime;
+      i++;
+    }while(currentHash.hasOwnProperty(hashValue));
+
+    return hashValue;
+  }
+
+  hashMethodQuadraticProbing = (value, prime, currentHash) => {
+
+    return value % prime;
+  }
+
+  hashMethodDoubleHashing = (value, prime, currentHash) => {
+
+    return value % prime;
+  }
 
   generatePrimeSet = length => {
     const possiblePrimes = [29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
